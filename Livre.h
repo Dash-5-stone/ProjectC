c71eefb1f427a76ee1ca9e116f86f5156e212cfb
Joseph HOUNKPE <151950672+Dash-5-stone@users.noreply.github.com>
2025-12-03 15:39:27 +0100

    Declaration de la classe Livre.h

---
+++ Livre.h
@@ -0,0 +1,38 @@
+#ifndef LIVRE_H
+#define LIVRE_H
+#include 'Date.h'
+#include <string>
+
+class Livre {
+private:
+std::string titre_;
+std::string auteur_;
+std::string genre_ ;
+Date datePublication_;
+int isbn_;
+std::string langue_;
+bool disponibilite_;
+std::list<std::string> identifiantEmprunteur_;
+
+
+public:
+Livre(std::string titre_, std::string auteur_, std::string langue_,Date datePublication_, std::string genre_, int isbn_, bool disponibilite_);
+Livre();
+std::string getTitre();
+std::string getAuteur();
+std::string getLangue();
+std::string getGenre();
+std::string setLangue();
+std::string setGenre();
+std::string setAuteur();
+int getIsbn();
+int setIsbn();
+bool getDisponibilite();
+bool setDisponibilite();
+
+
+};
+
+
+
+#endif
